5620cfa5012e5b06ca1b25bb8ccf534c6b151638
kentee-0428 <j1742769@icloud.com>
2023-03-06 21:01:25 +0900

    Fix 不要なファイル削除

--- db/migrate/20230224111132_add_columns_to_users 2.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :image, :string
-    add_column :users, :introduction, :string
-    add_column :users, :gender, :integer
-  end
-end
--- db/migrate/20230305103557_change_column_to_users 2.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToUsers < ActiveRecord::Migration[6.1]
-  def change
-    change_column :users, :gender, :string
-  end
-end
